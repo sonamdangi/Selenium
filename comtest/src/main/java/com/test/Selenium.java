9baf947f75f779d3a0d8de4a0732ac2e1f95bb44
Gaurav Ubnare <gauravubnare123@gmail.com>
2019-07-02 22:35:47 +0800

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -1,14 +1,11 @@
 package com.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Selenium {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/chromedriver"); // <-- Change this path
-        ChromeOptions chromeOptions = new ChromeOptions();
         ChromeOptions.addArguments("--headless");
         String baseUrl = "https://experitest.com/free-trial/";
         String expectedTitle = "Free trial";
