67513723d6a95040bfd9ec4b63e1fab480f487b2
Gaurav Ubnare <gauravubnare123@gmail.com>
2019-07-02 22:35:06 +0800

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -1,9 +1,6 @@
 package com.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
