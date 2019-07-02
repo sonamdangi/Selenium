bfd1c3ceee1d86c7d4712a1752dc815df5009f67
Gaurav Ubnare <gauravubnare123@gmail.com>
2019-07-02 23:25:54 +0800

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -7,6 +7,7 @@ public class Selenium {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/chromedriver"); // <-- Change this path
         WebDriver driver = new ChromeDriver();
+        chromeOptions.addArguments("--headless");
         String baseUrl = "https://experitest.com/free-trial/";
         String expectedTitle = "Free trial";
         String actualTitle = "";
