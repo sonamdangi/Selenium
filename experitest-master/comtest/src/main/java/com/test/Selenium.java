9946175cdc6cd2ba37a6272b6e742d819b172389
sonamdangi <42836307+sonamdangi@users.noreply.github.com>
2019-07-05 13:07:44 -0400

    Update Selenium.java

--- experitest-master/comtest/src/main/java/com/test/Selenium.java
+++ experitest-master/comtest/src/main/java/com/test/Selenium.java
@@ -6,17 +6,15 @@ public class Selenium {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/var/lib/jenkins/chromedriver"); // <-- Change this path
-        WebDriver driver = new ChromeDriver();
-        String baseUrl = "https://experitest.com/free-trial/";
-        String expectedTitle = "Free trial";
-        String actualTitle = "";
-        driver.get(baseUrl);
-        actualTitle = driver.getTitle();
-        if (actualTitle.contentEquals(expectedTitle)){
-            System.out.println("TEST PASSED!");
-        } else {
-            System.out.println("TEST FAILED");
-        }
-        driver.close();
+         // Add options to Google Chrome. The window-size is important for responsive sites
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+      //  options.addArguments("window-size=1200x600");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://seleniumhq.org");
+        // a guarantee that the test was really executed
+        assertTrue(driver.findElement(By.id("q")).isDisplayed());
+      
+        driver.quit();
     }
 }
